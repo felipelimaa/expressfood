4a9bf348a163d93b73954453d8a88424e6b61e2a
Felipe Lima <fearlimasi@gmail.com>
2021-08-28 13:11:20 -0300

    feat: iniciado criacao dos domains

---
+++ src/main/groovy/br/com/felipelima/api/expressfood/domain/model/Cozinha.groovy
@@ -0,0 +1,22 @@
+package br.com.felipelima.api.expressfood.domain.model
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+@ToString(includePackage = false,includeNames = true)
+@EqualsAndHashCode
+class Cozinha {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="cozinha_id")
+    Long id
+
+    String nome
+}
