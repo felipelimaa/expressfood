0047eda22699725e44b565c54a31b6fb985d3ff7
Felipe Lima <fearlimasi@gmail.com>
2021-08-28 12:31:20 -0300

    Commit inicial

---
+++ src/main/groovy/br/com/felipelima/api/expressfood/ExpressfoodApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.felipelima.api.expressfood
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ExpressfoodApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ExpressfoodApplication, args)
+	}
+
+}
---
+++ src/test/groovy/br/com/felipelima/api/expressfood/ExpressfoodApplicationTests.groovy
@@ -0,0 +1,13 @@
+package br.com.felipelima.api.expressfood
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExpressfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
