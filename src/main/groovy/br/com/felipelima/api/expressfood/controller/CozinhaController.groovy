b8c290c032c427f6d39cdd1df1410a7927129d61
Felipe Lima <fearlimasi@gmail.com>
2021-09-02 21:00:16 -0300

    fix: refatorado uri para deixa-las no plural

--- src/main/groovy/br/com/felipelima/api/expressfood/controller/CozinhaController.groovy
+++ src/main/groovy/br/com/felipelima/api/expressfood/controller/CozinhaController.groovy
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/cozinha")
+@RequestMapping("/cozinhas")
 class CozinhaController {
     @Autowired
     CozinhaService cozinhaService
