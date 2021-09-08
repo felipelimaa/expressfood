3321624d11b85e20ff5edbaafef4eee1b37a9b66
Felipe Lima <fearlimasi@gmail.com>
2021-09-07 22:59:23 -0300

    feat: implementado metodo patch para o Restaurante

--- src/main/groovy/br/com/felipelima/api/expressfood/api/controller/RestauranteController.groovy
+++ src/main/groovy/br/com/felipelima/api/expressfood/api/controller/RestauranteController.groovy
@@ -3,17 +3,22 @@ package br.com.felipelima.api.expressfood.api.controller
 import br.com.felipelima.api.expressfood.domain.exception.EntidadeNotFoundException
 import br.com.felipelima.api.expressfood.domain.model.Restaurante
 import br.com.felipelima.api.expressfood.domain.service.RestauranteService
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.util.ReflectionUtils
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
+import java.lang.reflect.Field
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -29,12 +34,12 @@ class RestauranteController {
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<?> get(@PathVariable Long id){
+    ResponseEntity<Restaurante> get(@PathVariable Long id){
         try {
             Restaurante restaurante = restauranteService.get(id)
             return ResponseEntity.ok(restaurante)
         } catch(EntidadeNotFoundException e){
-            return ResponseEntity.status(e.status).body(e.message)
+            return ResponseEntity.status(e.status).body(e.message) as ResponseEntity<Restaurante>
         }
     }
 
@@ -58,6 +63,15 @@ class RestauranteController {
         }
     }
 
+    @PatchMapping("/{id}")
+    ResponseEntity<?> partialUpdate(@RequestBody Map<String, Object> campos, @PathVariable Long id){
+        Restaurante restauranteExists = restauranteService.get(id)
+
+        merge(campos, restauranteExists)
+
+        return update(restauranteExists, id)
+    }
+
     @DeleteMapping("/{id}")
     ResponseEntity<?> remove(@PathVariable Long id){
         try {
@@ -66,6 +80,21 @@ class RestauranteController {
         } catch(EntidadeNotFoundException e){
             return ResponseEntity.status(e.status).body(e.message)
         }
+    }
+
+    private merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino){
+        ObjectMapper objectMapper = new ObjectMapper()
+        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante)
+
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            Field field = ReflectionUtils.findField(Restaurante, nomePropriedade)
+            field.setAccessible(true)
+
+            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem)
+
+            ReflectionUtils.setField(field, restauranteDestino, novoValor)
+        })
+
     }
 
 }
