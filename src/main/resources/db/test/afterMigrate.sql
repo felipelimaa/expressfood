78f86aed039466b1623f4b87392143c58b6208d0
felipelima <fearlimasi@gmail.com>
2021-12-20 22:30:16 -0300

    fix: ajustado fluxo do flyway para criação dos objetos no banco e configurações de execução do flyway no application.properties

--- src/main/resources/db/test/afterMigrate.sql
+++ src/main/resources/db/test/afterMigrate.sql
@@ -1,6 +1,42 @@
+-- Desabilita checagem de foreign keys para poder fazer a limpeza da base
+set foreign_key_checks = 0;
+
+-- Limpa todas as tabelas
+delete from estado;
+delete from cidade;
+delete from cozinha;
+delete from restaurante;
+delete from produto;
+delete from forma_pagamento;
+delete from restaurante_forma_pagamento;
+delete from permissao;
+delete from usuario;
+delete from grupo;
+delete from grupo_permissao;
+delete from usuario_grupo;
+delete from pedido;
+delete from item_pedido;
+
+-- Habilita checagem de foreign keys para poder fazer a limpeza da base
+set foreign_key_checks = 1;
+
+-- Reseta as sequences
+alter table estado auto_increment = 1;
+alter table cidade auto_increment = 1;
+alter table cozinha auto_increment = 1;
+alter table restaurante auto_increment = 1;
+alter table produto auto_increment = 1;
+alter table forma_pagamento auto_increment = 1;
+alter table permissao auto_increment = 1;
+alter table usuario auto_increment = 1;
+alter table grupo auto_increment = 1;
+alter table pedido auto_increment = 1;
+alter table item_pedido auto_increment = 1;
+
+
 -- Estado
-insert into estado (id, nome, uf) values (1, 'Rio Grande do Norte', 'RN');
-insert into estado (id, nome, uf) values (2, 'Paraíba', 'PB')
+insert into estado (id, nome_estado, uf) values (1, 'Rio Grande do Norte', 'RN');
+insert into estado (id, nome_estado, uf) values (2, 'Paraíba', 'PB');
 
 -- Cidade
 insert into cidade (id, nome, estado_id) values (1, 'Natal', 1);
@@ -20,9 +56,9 @@ insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, e
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (3, 'Lanchonete do Tio Sam', 11, 3, utc_timestamp, utc_timestamp);
 
 -- Produtos do Restaurante
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Carne Assada', null, 10.99, 1, 1);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Picanha', null, 69.99, 1, 1);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Feijão Verde', null, 6.99, 1, 2);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Carne Assada', 'Carne assada na brasa', 10.99, 1, 1);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Picanha', 'Suculenta picanha', 69.99, 1, 1);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Feijão Verde', 'O melhor feijão verde', 6.99, 1, 2);
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Porco com molho agridoce', 'Deliciosa carne suína ao molho especial', 78.90, 1, 1);
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Camarão tailandês', '16 camarões grandes ao molho picante', 110, 1, 1);
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Salada picante com carne grelhada', 'Salada de folhas com cortes finos de carne bovina grelhada e nosso molho especial de pimenta vermelha', 87.20, 1, 2);
@@ -43,9 +79,21 @@ insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Pe
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
 
 -- Formas de Pagamento associados ao Restaurante
+delete from restaurante_forma_pagamento;
 insert into restaurante_forma_pagamento values (1, 1);
 insert into restaurante_forma_pagamento values (1, 2);
 insert into restaurante_forma_pagamento values (1, 3);
 insert into restaurante_forma_pagamento values (2, 2);
 insert into restaurante_forma_pagamento values (2, 3);
 
+-- Usuarios
+
+-- Grupos de usuarios
+
+-- Permissao dos grupos
+
+-- Grupo associado ao usuario
+
+-- Pedido
+
+-- Item do Pedido
