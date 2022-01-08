915bcd9e3aa9dbe55b0b25824939800ed834f2e0
Felipe Lima <fearlimasi@gmail.com>
2022-01-08 15:50:13 -0300

    fix: adicionado duas propriedades no application.properties para conseguir tratar na ApiExceptionHandler os exceptions de DispatcherServlet

--- src/main/groovy/br/com/felipelima/api/expressfood/api/exceptionHandler/ApiExceptionHandler.groovy
+++ src/main/groovy/br/com/felipelima/api/expressfood/api/exceptionHandler/ApiExceptionHandler.groovy
@@ -17,10 +17,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
+import org.springframework.web.servlet.DispatcherServlet
 import org.springframework.web.servlet.NoHandlerFoundException
-import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
+import javax.servlet.ServletRequest
 import java.time.LocalDateTime
 import java.util.stream.Collectors
 
