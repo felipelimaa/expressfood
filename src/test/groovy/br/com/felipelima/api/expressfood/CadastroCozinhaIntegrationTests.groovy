2d185a63543623bf004ff2ccf6d1d0e9d4bfc7a2
felipe <fearlimasi@gmail.com>
2022-01-27 20:26:50 -0300

    test: criação dos testes de Cozinha

---
+++ src/test/groovy/br/com/felipelima/api/expressfood/CadastroCozinhaIntegrationTests.groovy
@@ -0,0 +1,70 @@
+package br.com.felipelima.api.expressfood
+
+import br.com.felipelima.api.expressfood.domain.exception.EntidadeEmUsoException
+import br.com.felipelima.api.expressfood.domain.exception.EntidadeNotFoundException
+import br.com.felipelima.api.expressfood.domain.model.Cozinha
+import br.com.felipelima.api.expressfood.domain.service.CozinhaService
+import br.com.felipelima.api.expressfood.domain.service.RestauranteService
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+import javax.validation.ConstraintViolationException
+
+import static org.junit.jupiter.api.Assertions.*
+
+@SpringBootTest
+class CadastroCozinhaIntegrationTests {
+
+	@Autowired
+	CozinhaService cozinhaService
+
+	@Test
+	void cozinha_TestarCriacaoComSucesso() {
+		// cenário
+		Cozinha cozinha = new Cozinha(nome: "Chinesa")
+
+		// ação
+		Cozinha cozinhaCreated = cozinhaService.create(cozinha)
+
+		// validação
+		assertEquals(cozinhaCreated.nome, cozinha.nome)
+		assertTrue(cozinhaCreated.id > 0)
+	}
+
+	@Test
+	void cozinha_TestarCriacaoSemNome() {
+		Cozinha cozinha = new Cozinha(nome: null)
+		assertThrows(ConstraintViolationException.class, { cozinhaService.create(cozinha)  })
+	}
+
+	@Test
+	void cozinha_CriaERecuperaId() {
+		Cozinha cozinha = cozinhaService.create(new Cozinha(nome: "Chinesa"))
+		Cozinha cozinhaRecovery = cozinhaService.get(cozinha.id)
+		assertEquals(cozinhaRecovery.id, cozinha.id)
+		assertEquals(cozinhaRecovery.nome, cozinha.nome)
+	}
+
+	@Test
+	void cozinha_RecuperaIdInvalido() {
+		assertThrows(EntidadeNotFoundException.class, { cozinhaService.get(Integer.MAX_VALUE) })
+	}
+
+	@Test
+	void cozinha_ExcluirComSucesso() {
+		Cozinha cozinha = cozinhaService.create(new Cozinha(nome: "Chinesa"))
+		Cozinha cozinhaRemoved = cozinhaService.remove(cozinha.id)
+	}
+
+	@Test
+	void cozinha_ExcluirEmUso() {
+		assertThrows(EntidadeEmUsoException.class, { cozinhaService.remove(3)})
+	}
+
+	@Test
+	void cozinha_ExcluirInexistente() {
+		assertThrows(EntidadeNotFoundException.class, { cozinhaService.remove(Integer.MAX_VALUE) })
+	}
+
+}
